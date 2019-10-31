126f1ed0eb9ae2409d6ba37b0f17e848b8c6a0e0
Paul Traylor <kungfudiscomonkey@gmail.com>
2019-10-31 21:15:37 +0900

    Test fix for Upstream.Host

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,6 +1,7 @@
 package proxy
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 
@@ -15,9 +16,24 @@ type Proxy struct {
 	Config config.VirtualHost
 }
 
+type DebugTransport struct{}
+
+func (DebugTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	b, err := httputil.DumpRequestOut(r, false)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(b))
+	return http.DefaultTransport.RoundTrip(r)
+}
+
 // ProxyRequest to upstream Prometheus server
 func (p *Proxy) ProxyRequest(w http.ResponseWriter, req *http.Request) {
 	proxy := httputil.NewSingleHostReverseProxy(p.Config.Prometheus.Upstream.URL)
+	req.Host = p.Config.Prometheus.Upstream.Host
 	level.Info(p.Logger).Log("msg", "proxying request", "upstream", p.Config.Prometheus.Upstream, "query", req.URL.String())
+
+	// proxy.Transport = DebugTransport{}
+
 	proxy.ServeHTTP(w, req)
 }
